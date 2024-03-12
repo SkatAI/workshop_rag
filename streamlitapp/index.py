abe186010612b48fda07cde93c516f1d83effc6a
Alexis Perrier <alexis.perrier@gmail.com>
2024-03-12 14:24:19 +0100

    update

--- streamlitapp/index.py
+++ streamlitapp/index.py
@@ -1,7 +1,17 @@
 import streamlit as st
-
+from  sklearn.datasets import load_iris
 
 st.title('RAG workshop')
-st.header("workshop sur le RAG sur l'union européenne")
 
-st.write("Hello world")
+
+with st.sidebar:
+    st.write("Bonjour")
+
+
+    with st.form("search_form", clear_on_submit=False):
+        search_query = st.text_area("Votre question:",
+            key="query_input",
+            height=20,
+            help="""Write a query, a question about your dataset""")
+
+        search_button = st.form_submit_button(label="Ask")
