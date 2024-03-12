98ba5b0d64f67976a2a5e121432e9c12e1c0e104
Alexis Perrier <alexis.perrier@gmail.com>
2024-03-12 14:27:59 +0100

    wip

--- streamlitapp/index.py
+++ streamlitapp/index.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from  sklearn.datasets import load_iris
 
 st.title('RAG workshop')
 
@@ -8,10 +7,15 @@ with st.sidebar:
     st.write("Bonjour")
 
 
-    with st.form("search_form", clear_on_submit=False):
-        search_query = st.text_area("Votre question:",
-            key="query_input",
-            height=20,
-            help="""Write a query, a question about your dataset""")
+with st.form("search_form", clear_on_submit=False):
+    search_query = st.text_area("Votre question:",
+        key="query_input",
+        height=20,
+        help="""Write a query, a question about your dataset""")
 
-        search_button = st.form_submit_button(label="Ask")
+    search_button = st.form_submit_button(label="Ask")
+
+
+if search_button:
+    #  rajouter ici tous le process de la question
+    st.write(f"your query: {search_query}")
